edda03687f456d1262db725594dff3c0f7a6e321
angelcoding06 <vasquezgerenaangelmateo@gmail.com>
2024-11-01 16:12:53 -0500

    Taller1 Hello wordl!

---
+++ Taller1/app/src/main/java/com/example/taller1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.taller1;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView helloTextView = findViewById(R.id.helloTextView);
+
+    }
+}
